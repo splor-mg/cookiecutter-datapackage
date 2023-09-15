f8a4c60d80401b4dc592f8748fcc0b684822a1b8
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-15 09:31:22 -0300

    fix missing f-string

--- {{ cookiecutter.project_slug }}/scripts/extract.py
+++ {{ cookiecutter.project_slug }}/scripts/extract.py
@@ -3,4 +3,4 @@ import logging
 logger = logging.getLogger(__name__)
 
 def extract_resource(resource_name: str, descriptor: str = 'datapackage.yaml'):
-    logger.info('Extract not implemented for resource {resource_name}...')
+    logger.info(f'Extract not implemented for resource {resource_name}...')
